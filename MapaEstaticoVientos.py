632b2bcd748ff03c1d78df3e22850192610a9bde
C-Holguin <crislholguin@gmail.com>
2025-04-29 11:41:17 -0300

    Ahora funciona :)

--- MapaEstaticoVientos.py
+++ MapaEstaticoVientos.py
@@ -10,10 +10,10 @@ path = '/volumen/files/'
 fecha = datetime.datetime.now()
 
 # Extraer fecha de archivo deseado
-year = 2024#fecha.year
-month = 10#fecha.month
-day = 31#fecha.day
-hour = 0#(6*((fecha.hour-3)//6) - 6) % 24 #redondear a ultima hora Arg con datos (00, 06, 12 o 18)
+year = fecha.year
+month = fecha.month
+day = fecha.day
+hour = (6*((fecha.hour-3)//6) - 6) % 24 #redondear a ultima hora Arg con datos (00, 06, 12 o 18)
 forecast = 0
 
 # Crear el objeto datetime con los datos extraídos
@@ -26,7 +26,7 @@ print(f"Fecha inicial extraída del archivo: {INIT_DATE}")
 # Descargar archivo del SMN y Abrir archivo descargado
 #smn_nc = datos_met_x_fecha(year= year, month= month, day= day, hour= hour, forecast= forecast, local_path = path)
 #ds = xr.open_dataset(smn_nc, decode_coords = 'all', engine = 'h5netcdf')
-file = '/volumen/files/Almacenamiento/Aggregated_Data_compressed9.nc'
+file = '/volumen/files/Almacenamiento/Aggregated_Data_compressed9.nc'############ CAMBIAR A FUNCION ######################
 ds = xr.open_dataset(file, decode_coords = 'all', engine = 'h5netcdf')
 
 print(ds)
@@ -90,44 +90,47 @@ ds.to_netcdf('ds.nc')
 
 
 #### VISUALIZACION EN MAPA INTERACTIVO ####
+# Access the 'time' variable from the dataset
+time_var = ds['time']
 
-## Cargar mapa base de ipyleaflet
-m = Map(center=(-37.5, -62), zoom=5, layout= Layout(height ='100vh'), layer_ctrl=True, basemap=basemaps.CartoDB.DarkMatter)
+# Get the units attribute from the time variable
+time_units = time_var.attrs.get('units')
 
-##Codigo geemap para cargar datos .nc (https://geemap.org/geemap/?h=add_velocity#geemap.geemap.Map.add_velocity)
-
-
-ds = ds.sel(time=INIT_DATE).squeeze()###ELIMINAR
-
-##Eliminar dimension time para que funcione codigo
-#if "time" in list(ds.coords.keys()):
-#    ds = ds.isel(time=INIT_DATE, drop=True)
+# Convert the numerical time values to datetime objects using num2date
+time_values = nc.num2date(time_var[:], time_units)
 
 
-# Normalizar para colormap [0, 1]
-norm = Normalize(vmin = 0, vmax = 50, clip = True)
+# Print the time values
+for time in time_values:
+    print(time)
 
-#Mapa de color
-#colores = ["#5e5a77", "#5b5b9d", "#4285ba", "#51ac92", "#4dc14a", "#90cb4d", "#cbd73f", "#d9c03e", "#d89d46", "#d4774b", "#cb4a73", "#a3355b", "#901c4f", "#631a1b", "#2b0001", "#000000", "#000000", "#000000", "#000000"]
-#cmap = ListedColormap(colores)
+for fecha in fechas:
+    ## Cargar mapa base de ipyleaflet
+    m = Map(center=(-37.5, -62), zoom=5, layout= Layout(height ='100vh'), layer_ctrl=True, basemap=basemaps.CartoDB.DarkMatter)
+    ##Codigo geemap para cargar datos .nc (https://geemap.org/geemap/?h=add_velocity#geemap.geemap.Map.add_velocity)
 
-# Crear mapa de fondo desde magnitud del viento
-fig, ax = plt.subplots(figsize=(8, 6))
-mesh = ax.pcolormesh(ds['lon'].values, ds['lat'].values, norm(ds['magViento10'].values), cmap = "viridis", shading='auto')
-ax.axis('off')  
+    ds = ds.sel(time=fecha).squeeze()###ELIMINAR
 
-# Exportar como .png
-img_name = "fondo_viento.png"
-plt.savefig(path + img_name, format="png", bbox_inches='tight', pad_inches=0)
-plt.close(fig)
+    # Normalizar para colormap [0, 1]
+    norm = Normalize(vmin = 0, vmax = 50, clip = True)
+
+    # Crear mapa de fondo desde magnitud del viento
+    fig, ax = plt.subplots(figsize=(8, 6))
+    mesh = ax.pcolormesh(ds['lon'].values, ds['lat'].values, norm(ds['magViento10'].values), cmap = "viridis", shading='auto')
+    ax.axis('off')  
+
+    # Exportar como .png
+    img_name = "fondo_viento.png"
+    plt.savefig(path + img_name, format="png", bbox_inches='tight', pad_inches=0)
+    plt.close(fig)
 
 
-# 2. Cargar .png a mapa ipyleaflef con coordenadas limite
-m.add_layer(ImageOverlay(url=img_name, bounds=[[lat_min, lon_min], [lat_max, lon_max]], opacity=0.6)) 
+    # 2. Cargar .png a mapa ipyleaflef con coordenadas limite
+    m.add_layer(ImageOverlay(url=img_name, bounds=[[lat_min, lon_min], [lat_max, lon_max]], opacity=0.6)) 
 
-##Generar mapa de velocidad de viento
-m.add(Velocity(data = ds, zonal_speed = "u_wind",  meridional_speed='v_wind', velocity_scale = 0.005, color_scale = "white")) #velocity_scale cambia largo de lineas graficadas
+    ##Generar mapa de velocidad de viento
+    m.add(Velocity(data = ds, zonal_speed = "u_wind",  meridional_speed='v_wind', velocity_scale = 0.005, color_scale = "white")) #velocity_scale cambia largo de lineas graficadas
 
-##Exportar mapa a html
-archivo = path + f'vientos_smn_{INIT_DATE.date()}_{INIT_DATE.hour}.html'
-m.save(archivo, title='Mapa de Vientos - IGN')
+    ##Exportar mapa a html
+    archivo = path + f'vientos_smn_{fecha.date()}_{fecha.hour}.html'
+    m.save(archivo, title='Mapa de Vientos - IGN')
